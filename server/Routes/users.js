eb1a2fdec9de1c868416d10550dd185448ecf1a3
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-01-08 22:18:15 +0530

    Update users.js

--- server/Routes/users.js
+++ server/Routes/users.js
@@ -9,13 +9,14 @@ const saltRounds = 10;
 
 router.post("/register", async (req, res) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
         const newUser = new User({
             username : req.body.username,
             email : req.body.email,
             password : hashedPassword,
         });
         const savedUser = await newUser.save();
-        res.status(200).json(savedUser);
+        res.status(200).json();
     }catch(err) {
         console.log(err);
         res.status(500).json(err);
