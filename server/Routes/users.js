b6aeab12f4e44a8aa1255d69046147f27bb68e89
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-01-08 22:17:28 +0530

    Update users.js

--- server/Routes/users.js
+++ server/Routes/users.js
@@ -1,1 +1,23 @@
 
+const router = require("express").Router();
+const User = require('../Models/User');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+// Register
+
+router.post("/register", async (req, res) => {
+    try {
+        
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    }catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Login
+
+module.exports = router;
