6e7bf8e17406f32c9d7d5c8d4c3e0f76d15301a0
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-01-10 19:11:59 +0530

    Update Pin.js

--- server/Models/Pin.js
+++ server/Models/Pin.js
@@ -1,1 +1,38 @@
 
+const mongoose = require("mongoose");
+
+const pinSchema = new mongoose.Schema({
+    
+    username: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        min: 2,
+    },
+    desc: {
+        type: String,
+        required: true,
+        min : 3
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+    },
+    lat: {
+        type: Number,
+        required: true,
+    },
+    lon: {
+        type: Number,
+        required: true,
+    },
+},
+    {timestamps:true}
+);
+
+module.exports = new mongoose.model("Pin", pinSchema);
