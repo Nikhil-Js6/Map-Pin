9d6f012c8505a064eae0aeea5a255dc750148c36
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-01-13 10:36:57 +0530

    Update pins.js

--- server/Routes/pins.js
+++ server/Routes/pins.js
@@ -1,1 +1,26 @@
+const router = require("express").Router();
+const Pin = require('../Models/Pin');
 
+
+// Create a Pin
+
+router.post("/", async (req, res) => {
+    const newPin = new Pin(req.body);
+    try {
+        const savedPin = await newPin.save();
+    }catch(err) {
+        console.log(err);
+    }
+});
+
+// Get all Pins
+
+router.get("/", async (req, res) => {
+    try {
+        const pins = await Pin.find();
+    }catch(err) {
+        console.log(err);
+    }
+});
+
+module.exports = router;
